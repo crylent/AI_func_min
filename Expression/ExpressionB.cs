158e318866b18827f6c892e4e0dd3894c31b7c53
Anna <0301annaumnova@gmail.com>
2023-10-12 19:04:26 +0300

    expressions 15 13

---
+++ Expression/ExpressionB.cs
@@ -0,0 +1,7 @@
+﻿namespace AI_func_min.Expression;
+
+public class ExpressionB:IMathExpression
+{
+    public float Calculate(float x1, float x2) =>
+         float.Pow(x2 - float.Pow(x1, 2), 2) + 100 * float.Pow(1 - x1, 2);
+}
---
+++ Expression/ExpressionC.cs
@@ -0,0 +1,7 @@
+﻿namespace AI_func_min.Expression;
+
+public class ExpressionC:IMathExpression
+{
+    public float Calculate(float x1, float x2) =>
+       4 * float.Pow(x1 - 5, 2) + float.Pow(x2 - 6, 2);
+}
